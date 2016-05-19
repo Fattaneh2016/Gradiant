46c13ed7b95f6e309043b79fcf566ec49f8f48da
Fattaneh2016 <fatameri@hotmail.com>
2016-05-18 22:41:02 -0400

    add Gradiant

---
+++ Script.js
@@ -0,0 +1,25 @@
+function initializa(){
+
+    var elem = document.getElementById('myCanvas');
+
+    var canvas = elem.getContext('2d');
+
+   canvas.fillStyle="#FF0000";
+   canvas.strokeStyle = "#0000AA";
+   
+    canvas.strokeRect(100, 100, 120, 120);
+    canvas.fillRect(110, 110, 100, 100);
+    canvas.clearRect(130, 130, 30, 30);
+
+    
+    
+    var grd=canvas.createLinearGradient(0,0,170,0);
+    grd.addColorStop(0,"red");
+    grd.addColorStop(1,"blue");
+
+    canvas.fillStyle=grd;
+    canvas.fillRect(20,20,150,100);
+  //  canvas.fillRect(100, 100, 200, 100);
+}
+
+addEventListener("load", initializa)
